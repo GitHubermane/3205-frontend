6a4706f9784bd076ec48fb7f541d8aaad11a28e0
Viktor Kryuchkov <crykour12@gmail.com>
2023-12-01 20:30:32 +0300

    add Input

---
+++ src/components/Input/index.tsx
@@ -0,0 +1,58 @@
+import type { ChangeEvent, FC } from 'react';
+import { useId } from 'react';
+import { InputType } from 'types';
+import styles from './styles.module.scss';
+
+type PropsType = {
+  value: string;
+  label: string;
+  onChange: (value: string) => void;
+  type?: InputType;
+};
+
+export const Input: FC<PropsType> = ({
+  label,
+  onChange,
+  value,
+  type = 'text',
+}) => {
+  const id = useId();
+  // Для обычного текста
+  const onValueChange = (e: ChangeEvent<HTMLInputElement>) =>
+    onChange(e.currentTarget.value);
+
+  // Для номера телефона
+  const onTelephoneChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const telephone = e.currentTarget.value.replace(/[^0-9]/g, ''); // Удаляем все, кроме цифр
+    let formattedTelephone = '';
+
+    for (let i = 0; i < telephone.length; i += 2) {
+      formattedTelephone += `${telephone.slice(i, i + 2)}-`;
+    }
+
+    formattedTelephone = formattedTelephone.slice(0, -1); // Убираем последний дефис
+
+    onChange(formattedTelephone);
+  };
+
+  const isTelephone = type === InputType.telephone;
+
+  return (
+    <div className={styles.input__block}>
+      <label
+        className={styles.input__label}
+        htmlFor={id}
+      >
+        {label}
+      </label>
+      <input
+        value={value}
+        onChange={isTelephone ? onTelephoneChange : onValueChange}
+        className={styles.input__body}
+        type="email"
+        placeholder={`Enter your ${label}`}
+        id={id}
+      />
+    </div>
+  );
+};
