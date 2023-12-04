e6c94a686a6646432bfe987c231f9cdd48c1c80f
Viktor Kryuchkov <crykour12@gmail.com>
2023-12-04 16:57:52 +0300

    add layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,10 @@
-function App() {
-  return <div>Hello world?</div>;
-}
+import { Layout } from 'components';
+import { InputForm } from 'container';
+
+const App = () => (
+  <Layout>
+    <InputForm />
+  </Layout>
+);
 
 export default App;
