41a23337b093528df928775e3d233ce4efdc55f4
Viktor Kryuchkov <crykour12@gmail.com>
2023-12-04 16:52:35 +0300

    add user component

---
+++ src/components/User/index.tsx
@@ -0,0 +1,18 @@
+import type { FC } from 'react';
+import styles from './styles.module.scss';
+
+type PropsType = {
+  number: string;
+  email: string;
+};
+
+export const User: FC<PropsType> = ({ email, number }) => (
+  <div className={styles.user}>
+    <div>
+      Email: <span>{email}</span>
+    </div>
+    <div>
+      Phone number: <span>{number}</span>
+    </div>
+  </div>
+);
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,4 @@
 export { Input } from './Input';
 export { Button } from './Button';
 export { Layout } from './Layout';
+export { User } from './User';
